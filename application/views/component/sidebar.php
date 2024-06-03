30d3beff558caf2223da9a6a7341febe356d8066
ariphamzah <arifhamzah947>
2024-06-03 19:42:54 +0700

    tambah form claim

--- application/views/component/sidebar.php
+++ application/views/component/sidebar.php
@@ -32,7 +32,7 @@
       <ul class="treeview-menu">
         <li class="<?= ($nav == '0')?'active':'' ?>"><a href="<?= base_url('admin/form_barangmasuk') ?>"><i class="fa fa-circle-o"></i> Barang Masuk</a></li>
         <li class="<?= ($nav == '4')?'active':'' ?>"><a href="<?= base_url('admin/form_satuan') ?>"><i class="fa fa-circle-o"></i> Satuan Barang</a></li>
-        <li class="<?= ($nav == '9')?'active':'' ?>"><a href="<?= base_url('admin/form_claim_barang') ?>"><i class="fa fa-circle-o"></i> Claim Barang</a></li>
+        <li class="<?= ($nav == '9')?'active':'' ?>"><a href="<?= base_url('admin/form_claimbarang') ?>"><i class="fa fa-circle-o"></i> Claim Barang</a></li>
       </ul>
     </li>
 
